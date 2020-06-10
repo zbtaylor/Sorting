3b4dccfb378da4ad10d67f2acf8eb30bb6216930
Zach Taylor <zbtaylor1@gmail.com>
2020-06-09 22:41:51 -0400

    merge sort complete

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,11 +1,15 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
+# look in to .extend() method for lists
+
+
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
-    # TO-DO
     a = 0
     b = 0
     m = 0
+
+    # go through both arrays while we can
     while a < len(arrA) and b < len(arrB):
         if arrA[a] <= arrB[b]:
             merged_arr[m] = arrA[a]
@@ -16,11 +20,13 @@ def merge(arrA, arrB):
             b += 1
             m += 1
 
+    # go through array A if anything remains
     while a < len(arrA):
         merged_arr[m] = arrA[a]
         a += 1
         m += 1
 
+    # go through array B if anything remains
     while b < len(arrB):
         merged_arr[m] = arrB[b]
         b += 1
@@ -29,15 +35,15 @@ def merge(arrA, arrB):
     return merged_arr
 
 
-arr1 = [1, 2, 3]
-arr2 = [4, 5, 6, 7]
-arr3 = merge(arr1, arr2)
-print(arr3)
-
-
 def merge_sort(arr):
     # TO-DO
-
+    # no more than 10 lines
+    # recursive
+    if len(arr) > 1:
+        middle = round(len(arr) / 2)
+        left = merge_sort(arr[:middle])
+        right = merge_sort(arr[middle:])
+        return merge(left, right)
     return arr
 
 
